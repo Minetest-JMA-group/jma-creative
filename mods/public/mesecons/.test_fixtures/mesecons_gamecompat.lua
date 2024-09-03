18f1d186bc0d10a01fd93e02f43b19fe8d8bcd02
Marko Petrović <petrovicmarko2006@gmail.com>
2024-09-03 21:56:45 +0200

    mesecons: Remove mod

--- mods/public/mesecons/.test_fixtures/mesecons_gamecompat.lua
+++
@@ -1,5 +0,0 @@
-fixture("mesecons")
-
-mineunit:set_current_modname("mesecons_gamecompat")
-mineunit:set_modpath("mesecons_gamecompat", "../mesecons_gamecompat")
-sourcefile("../mesecons_gamecompat/init")
--- mods/public/mesecons/.test_fixtures/screwdriver.lua
+++
@@ -1,6 +0,0 @@
-mineunit:set_current_modname("screwdriver")
-
-screwdriver = {}
-
-screwdriver.ROTATE_FACE = 1
-screwdriver.ROTATE_AXIS = 2
